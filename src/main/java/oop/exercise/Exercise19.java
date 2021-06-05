147bdb53e1b7c7ac6cd02a80778ee90634bfecb9
joshfeds14 <joshfeds14@gmail,com>
2021-06-05 13:42:01 -0400

    Exercise 19

---
+++ src/main/java/oop/exercise/Exercise19.java
@@ -0,0 +1,13 @@
+package oop.exercise;
+import java.util.Scanner;
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Joshua Federman
+ */
+public class Exercise19
+{
+    static Scanner input = new Scanner(System.in);
+    public static void main( String[] args ) {
+
+    }
+}
